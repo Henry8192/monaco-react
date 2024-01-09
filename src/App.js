c76deac5235ea2f9f4dc23b0be5a331bdf79f806
Henry <50559854+Henry8192@users.noreply.github.com>
2024-01-09 15:56:49 -0500

    perform a code clean up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import * as monaco from 'monaco-editor';
 // or import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 // if shipping only a subset of the features & languages is desired
 
 
 function MonacoEditor() {
-  // const containerRef = useRef();
 
-  // useEffect(() => {
-  //   // if (null === containerRef.current) {
-  //     containerRef.current = document.getElementById('container');
-  //     monaco.editor.create(containerRef.current, {
-  //       value: 'console.log("Hello, world")',
-  //       language: 'javascript'
-  //     });
-  //   // }
-  // }, []);
+  useEffect(() => {
+    monaco.editor.create(document.getElementById('container'), {
+      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+      language: 'javascript'
+    });
+  }, []);
 
-useEffect(() => {
-  monaco.editor.create(document.getElementById('container'), {
-    value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-    language: 'javascript'
-  });
-}, []);
-// monaco.editor.create(document.getElementById('container'), {
-// 	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-// 	language: 'javascript'
-// });
-// ref={containerRef.current}
   return <div id="container"  style={{ height: '100vh' , width:'100vw'}} />;
 }
 
